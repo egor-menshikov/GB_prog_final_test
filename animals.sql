604406a6753ae31ea60649bcaffbc1803a59e295
Egor Menshikov <r3born.fs@gmail.com>
2024-01-05 18:26:19 +0300

    tasks 11-12 done

--- animals.sql
+++ animals.sql
@@ -153,3 +153,86 @@ FROM donkeys;
 -- 11.Создать новую таблицу “молодые животные” в которую попадут все
 -- животные старше 1 года, но младше 3 лет и в отдельном столбце с точностью
 -- до месяца подсчитать возраст животных в новой таблице
+DROP TABLE IF EXISTS young;
+CREATE TABLE young AS
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM dogs
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR)
+UNION
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM cats
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR)
+UNION
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM hamsters
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR)
+UNION
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM horses
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR)
+UNION
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM camels
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR)
+UNION
+SELECT name,
+    date_birth,
+    skills,
+    species_id,
+    TIMESTAMPDIFF(MONTH, date_birth, CURDATE()) AS age
+FROM donkeys
+WHERE date_birth BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR)
+    AND ADDDATE(CURDATE(), INTERVAL -1 YEAR);
+
+
+-- 12. Объединить все таблицы в одну, при этом сохраняя поля, указывающие на
+-- прошлую принадлежность к старым таблицам.
+SELECT d.name, d.date_birth, d.skills, dom.species, y.age
+FROM dogs AS d
+LEFT JOIN young AS y ON y.name = d.name
+LEFT JOIN domestic AS dom ON dom.id = d.species_id
+UNION 
+SELECT c.name, c.date_birth, c.skills, dom.species, y.age
+FROM cats AS c 
+LEFT JOIN young AS y ON y.name = c.name
+LEFT JOIN domestic AS dom ON dom.id = c.species_id
+UNION
+SELECT h.name, h.date_birth, h.skills, dom.species, y.age
+FROM hamsters AS h 
+LEFT JOIN young AS y ON y.name = h.name
+LEFT JOIN domestic AS dom ON dom.id = h.species_id
+UNION
+SELECT hor.name, hor.date_birth, hor.skills, p.species, y.age
+FROM horses AS hor 
+LEFT JOIN young AS y ON y.name = hor.name
+LEFT JOIN pack AS p ON p.id = hor.species_id
+UNION
+SELECT dk.name, dk.date_birth, dk.skills, p.species, y.age
+FROM donkeys AS dk 
+LEFT JOIN young AS y ON y.name = dk.name
+LEFT JOIN pack AS p ON p.id = dk.species_id;
